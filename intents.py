23356a7911d196044ef9e0c24d1cc65ee05c4fe1
LQQ <LQQ@example.com>
2025-10-01 12:50:57 +0800

    chore: update intent classificaion rule for seat lookup

--- intents.py
+++ intents.py
@@ -2,16 +2,15 @@
 
 import re
 
-def classify_intents(text: str) -> str:
+def classify_intents(text: str) -> list[str]:
+    """
+    Return list of intents.
+    Currently only distinguish seat lookup.
+    """
     intents = []
     if any(k in text for k in ["座","位","桌","找", "坐"]):
         intents.append("seat_lookup")
-    if any(k in text for k in ["地點","地址","哪裡"]):
-        intents.append("wedding_location")
-    if any(k in text for k in ["時間","幾點","日期"]):
-        intents.append("wedding_time")
-    if not intents:
-        intents.append("smalltalk")
+
     return intents
 
 def extract_keyword(text: str) -> str:
@@ -19,7 +18,7 @@ def extract_keyword(text: str) -> str:
         r"我要找", r"幫我找", r"請幫我找", r"找一下", r"查一下", r"查詢", r"查",
         r"請問", r"問", r"麻煩", r"謝謝",
         r"座位", r"位置", r"座", r"位", r"桌", r"坐在哪裡", r"坐在哪", r"在哪裡", r"在哪",
-        r"我的", r"我座", r"我坐", r"我", r"的"
+        r"我的", r"我座", r"我坐", r"我", r"的", r"的位子", r"的座位"
     ]
     pattern = "(" + "|".join(phrases) + ")"
     text = re.sub(pattern, "", text)
